68265d46c03439b97817f54bd5dcdeb3fcf1d733
Vinodh Kumar <vinodh.mahendra@gmail.com>
2020-12-15 16:23:36 +0530

    zuul api gateway

--- currency-conversion-service/src/main/java/io/pivotal/workshop/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/io/pivotal/workshop/CurrencyConversionServiceApplication.java
@@ -2,8 +2,9 @@ package io.pivotal.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+@EnableDiscoveryClient
 @EnableFeignClients("io.pivotal.workshop.proxy")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
--- netflix-zuul-api-gateway-server/src/main/java/io/pivotal/workshop/NetflixZuulApiGatewayServerApplication.java
+++ netflix-zuul-api-gateway-server/src/main/java/io/pivotal/workshop/NetflixZuulApiGatewayServerApplication.java
@@ -3,13 +3,14 @@ package io.pivotal.workshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
-public class CurrencyExchangeServiceApplication {
+public class NetflixZuulApiGatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
 	}
 
 }
